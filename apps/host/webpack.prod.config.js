a755074408b77744843368a04eab8cf0a0019f54
Henrique Custodia <martinshenrique8@gmail>
2022-08-28 19:03:33 -0300

    add remote url for production env

--- apps/host/webpack.prod.config.js
+++ apps/host/webpack.prod.config.js
@@ -2,14 +2,7 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  /*
-   * Remote overrides for production.
-   * Each entry is a pair of an unique name and the URL where it is deployed.
-   *
-   * e.g.
-   * remotes: [
-   *   ['app1', 'https://app1.example.com'],
-   *   ['app2', 'https://app2.example.com'],
-   * ]
-   */
+  remotes: [
+    ['remote', 'https://brilliant-quokka-462c1f.netlify.app']
+  ]
 });
